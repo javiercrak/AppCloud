b35bf08539dfea12a9399af9152d1cec74221619
Oscar Malagón <ojmalagon@gmail.com>
2015-09-17 15:09:56 -0500

    Se inicializa proyecto incluyendo Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartToolsApp::Application.config.secret_token = '18e1b961fbdc5f74b9c60f3a77278b2e9a9da910101fda33717d75e1ed18ccf5f9612d8a520789e1fecccfd03d1f8a6256ba50857571b2cf8a87463adfcd7a54'
